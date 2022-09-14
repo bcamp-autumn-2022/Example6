788859ae93deaf6bb09997c3c75117599013f14e
Pekka Alaluukas <Pekka.Alaluukas@oamk.fi>
2022-09-14 13:54:49 +0300

    Ready

--- Program.cs
+++ Program.cs
@@ -10,6 +10,23 @@ namespace Example6
     {
         static void Main(string[] args)
         {
+            Student objectStudent = new Student();
+            objectStudent.Fname = "Ann";
+            objectStudent.Lname = "Jones";
+            objectStudent.StudentGroup = "1A";
+
+            Teacher objectTeacher = new Teacher();
+            objectTeacher.Fname = "Lisa";
+            objectTeacher.Lname = "Smith";
+            objectTeacher.TeachingSubject = "English";
+
+            Console.WriteLine("Student: " + objectStudent.Fname);
+            Console.WriteLine("Student: " + objectStudent.Lname);
+            Console.WriteLine("Student: " + objectStudent.StudentGroup);
+
+            Console.WriteLine("Teacher " + objectTeacher.Fname);
+            Console.WriteLine("Teacher " + objectTeacher.Lname);
+            Console.WriteLine("Teacher " + objectTeacher.TeachingSubject);
         }
     }
 }
---
+++ Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example6
+{
+    internal class Student:Person
+    {
+        public string StudentGroup { get; set; }
+    }
+}
